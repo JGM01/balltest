cc98f5d0a7de128e395e67c48c7cfa0e654372c4
JGM01 <jacobgermanamccray@gmail.com>
2025-12-16 23:53:42 -0500

    setting up..

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,32 @@
 #include "GLFW/glfw3.h"
+#include "webgpu/webgpu_cpp.h"
 #include <cstdio>
 #include <cstdlib>
 
 constexpr int viewWidth = 512;
 constexpr int viewHeight = 512;
+
+// An instance creates an opening for the program
+// to communicate with the OS and determine the underlying
+// graphics APIs to use.
+wgpu::Instance instance;
+
+// An adapter is what lets you pick the actual hardware you
+// want to use. The adapter returns a *promise*, not a device. No
+// allocations, no shaders, just information about the potential device.
+wgpu::Adapter adapter;
+
+// Where code is ran, and failures can occur. Gives the ability to act.
+wgpu::Device device;
+
+void init() {
+  const auto timedWaitAnyFeature = wgpu::InstanceFeatureName::TimedWaitAny;
+  const auto instanceDescriptor = wgpu::InstanceDescriptor{
+      .requiredFeatureCount = 1,
+      .requiredFeatures = &timedWaitAnyFeature,
+  };
+  instance = wgpu::CreateInstance(&instanceDescriptor);
+}
 
 void start() {
   if (!glfwInit())
@@ -22,6 +45,7 @@ void start() {
 int main() {
   printf("Starting sim...\n");
 
+  init();
   start();
 
   return EXIT_SUCCESS;
